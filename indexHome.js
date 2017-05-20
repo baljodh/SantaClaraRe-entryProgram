2b58624eea3095f3f2fd9852284730f37659987b
baljodh <baljodh2012@gmail.com>
2017-05-19 22:21:32 -0700

    Add files via upload
    Initial Check in

---
+++ indexHome.js
@@ -0,0 +1,65 @@
+function changeColor(){
+	document.getElementById("inputPrimary").style.backgroundColor="#ffff99";
+}
+function changeDatecolor(){
+	document.getElementById("date1").style.backgroundColor="#ffff99"
+}
+ 
+
+function changeFcolor(){
+	document.getElementById("fName").style.backgroundColor="#ffff99"
+}
+function changeMcolor(){
+	document.getElementById("mName").style.backgroundColor="#ffff99"
+}
+function changeLcolor(){
+	document.getElementById("lName").style.backgroundColor="#ffff99"
+}
+function changeMacolor(){
+	document.getElementById("maidenName").style.backgroundColor="#ffff99"
+}
+function changeDate2color(){
+	document.getElementById("date2").style.backgroundColor="#ffff99"
+}
+function changepfncolor(){
+	document.getElementById("pfnNumber").style.backgroundColor="#ffff99"
+}
+function changecdcrcolor(){
+	document.getElementById("cdcrNumber").style.backgroundColor="#ffff99"
+}
+function changeAdultColor(){
+	document.getElementById("noOfAdults").style.backgroundColor="#ffff99"
+}
+function changeChildrenColor(){
+	document.getElementById("noOfChildrenn").style.backgroundColor="#ffff99"
+}
+function changeBoxColor(){
+	document.getElementById("appointmentBox").style.backgroundColor="#ffff99"
+}
+function changeotherReasonncolor(){
+	document.getElementById("otherReason").style.backgroundColor="#ffff99"
+}
+$('#date1 input').autotab_magic().autotab_filter('numeric');
+$('#date1 input').datepicker()
+
+function changessnInputtcolor(){
+	document.getElementById("ssnInputt").style.backgroundColor="#ffff99"
+}
+function changeMainTelncolor(){
+	document.getElementById("mainTelBox").style.backgroundColor="#ffff99"
+}
+function changecellTelcolor(){
+	document.getElementById("cellTelBox").style.backgroundColor="#ffff99"
+}
+function changeemailcolor(){
+	document.getElementById("emailBox").style.backgroundColor="#ffff99"
+}
+
+function changeSelfPaidcolor(){
+	document.getElementById("ifSelfPaidBox").style.backgroundColor="#ffff99"
+}
+$(document).ready(function(){
+	$("firstButton").click(function(){
+		$("formSecond").toggle();
+	}
+})
